cfd92a969f57c45565932726137f156bdfe94650
Navaneeth-K-P <me1180630@iitd.ac.in>
2020-10-19 19:27:47 +0530

    First page animations done

--- css/animation_text.js
+++ css/animation_text.js
@@ -1,32 +1,21 @@
-// Wrap every letter in a span
-var textWrapper = document.querySelector('.ml11 .letters');
-textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
+$(document).ready(function() {
+    //MyBlah("hello");
+  var count = 1;
+   var maxCount = 2;
+   textAnimate(count,maxCount);
+});
 
-anime.timeline({loop: true})
-  .add({
-    targets: '.ml11 .line',
-    scaleY: [0,1],
-    opacity: [0.5,1],
-    easing: "easeOutExpo",
-    duration: 700
-  })
-  .add({
-    targets: '.ml11 .line',
-    translateX: [0, document.querySelector('.ml11 .letters').getBoundingClientRect().width + 10],
-    easing: "easeOutExpo",
-    duration: 700,
-    delay: 100
-  }).add({
-    targets: '.ml11 .letter',
-    opacity: [0,1],
-    easing: "easeOutExpo",
-    duration: 600,
-    offset: '-=775',
-    delay: (el, i) => 34 * (i+1)
-  }).add({
-    targets: '.ml11',
-    opacity: 0,
-    duration: 1000,
-    easing: "easeOutExpo",
-    delay: 1000
-  });
+function textAnimate(count,maxCount) {
+    $(".text"+count).animate({
+       opacity: '0'
+    }, { duration: 900, queue: false });
+  
+    count++;
+    if (count > maxCount) {count = 1;}
+  
+    $(".text"+count).animate({
+       opacity: '1'
+    }, { duration: 900, queue: false });
+  
+    window.setTimeout(function() { textAnimate(count,maxCount) }, 3000);
+}
